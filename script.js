6005b6ed25dc2dddc1a4dfadde5f4dbccb94f34d
Anusha-Yadavv <anushakottam22@gmail.com>
2024-02-03 21:43:37 +0530

    YouTube clone

---
+++ script.js
@@ -0,0 +1,120 @@
+const API_KEY = "AIzaSyDqz4okz51FunzRBD2sVT6FTPoDsvt2SkQ";
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+let choicesContainerEle = document.getElementById("all-choices-container");
+let subscriptionsContainer = document.getElementById("subscriptions-container");
+let searchBtn=document.getElementById("searchBtn");
+
+const choicesArray = [
+  "All",
+  "Recruitment",
+  "Vocubulary",
+  "Data Science",
+  "Algorithms",
+  "Asian music",
+  "New to you",
+  "Vocubulary",
+  "Data Science",
+  "Algorithms",
+  "Music",
+  "Javascript",
+  "Computer progrmming",
+];
+
+//Subscriptions nestead array
+const subscriptionsArray = [
+  ["./assets/profile1.svg", "James Gouse"],
+  ["./assets/profile2.svg", "Alan Cooper"],
+  ["./assets/profile3.svg", "Alexis Sears"],
+  ["./assets/profile4.svg", "Jesica Lambert"],
+  ["./assets/profile5.svg", "Anna White"],
+  ["./assets/profile6.svg", "Skylar Dias"],
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetchVideos("coding languages", 20);
+});
+
+choicesArray.map((choice) => {
+  console.log("choice", choice);
+  let divEle = document.createElement("div");
+  // divEle.className="user-choice";
+  divEle.innerHTML = `
+  <h1 class="user-choice">${choice}</h1>
+  `;
+  choicesContainerEle.appendChild(divEle);
+});
+
+//Creating subscriptions profiles 
+function createSubscriptions(profile,name){
+  console.log(`${profile} and ${name}`)
+
+  let subDiv = document.createElement("div");
+  subDiv.className = "sidebar-options";
+  subDiv.innerHTML = `
+  <i class="icon"><img src=${profile} alt="Your Videos"></i>
+
+  <h5>${name}</h5>
+  `;
+  subscriptionsContainer.appendChild(subDiv);
+
+}
+
+let arrLen=subscriptionsArray.length;
+for(let i=0;i<arrLen;i++){
+  console.log("sub arr",subscriptionsArray[i]);
+  
+    createSubscriptions(subscriptionsArray[i][0],subscriptionsArray[i][1]);
+  
+}
+
+
+function scrollContainer(direction) {
+  const container = document.getElementById("all-choices-container");
+
+  if (direction === "right") {
+    container.style.transform = "translateX(50px)"; 
+  } else if (direction === "left") {
+    container.style.transform = "translateX(-50px)"; 
+  }
+}
+
+async function fetchVideos(searchQuery, maxResults) {
+  const response = await fetch(
+    `${BASE_URL}/search?key=${API_KEY}&q=${searchQuery}&maxResults=${maxResults}&part=snippet`
+  );
+  const data = await response.json();
+  // console.log("DATA ITEMS",data.items)
+  displayVideos(data.items);
+}
+
+function displayVideos(videos) {
+  const container = document.getElementById("video-gallery");
+  container.innerHTML = "";
+
+  videos.forEach((video) => {
+    const videoId = video.id.videoId;
+    const title = video.snippet.title;
+    const thumbnail = video.snippet.thumbnails.high.url;
+
+    const videoCard = document.createElement("div");
+    videoCard.className = "video-card";
+    videoCard.innerHTML = `
+            <a href="video.html?videoId=${videoId}" class="video-link">
+                <img src="${thumbnail}" alt="${title}" class="video">
+                <div class="video-content">
+
+                <h3 class="heading">${title}</h3>
+                </div>
+            </a>
+        `;
+    container.appendChild(videoCard);
+  });
+}
+
+
+searchBtn.addEventListener("click",()=>{
+  let searchBarEle=document.getElementById("searchBar");
+  let userSearching=searchBarEle.value;
+  console.log("USER SEARCHING",userSearching);
+  fetchVideos(userSearching,20);
+})
---
+++ video.js
@@ -0,0 +1,186 @@
+const API_KEY = "AIzaSyDqz4okz51FunzRBD2sVT6FTPoDsvt2SkQ";
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+
+const profilesArray = [
+  "./assets/profile1.svg",
+  "./assets/profile2.svg",
+  "./assets/profile3.svg",
+  "./assets/profile4.svg",
+  "./assets/profile5.svg",
+  "./assets/profile6.svg",
+];
+
+window.addEventListener("load", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const videoId = urlParams.get("videoId");
+
+  if (videoId) {
+    loadVideo(videoId);
+    loadComments(videoId);
+    loadVideoDetails(videoId);
+  } else {
+    console.error("No video ID found in URL");
+  }
+});
+
+function loadVideo(videoId) {
+  if (YT) {
+    new YT.Player("video-container", {
+      height: "500",
+      width: "1000",
+      videoId: videoId,
+    });
+  }
+}
+
+async function loadVideoDetails(videoId) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/videos?key=${API_KEY}&part=snippet&id=${videoId}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data.items && data.items.length > 0) {
+      const channelId = data.items[0].snippet.channelId;
+      loadChannelInfo(channelId);
+    }
+  } catch (error) {
+    console.error("Error fetching video details: ", error);
+  }
+}
+
+async function loadChannelInfo(channelId) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/channels?key=${API_KEY}&part=snippet&id=${channelId}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data.items) {
+      displayChannelInfo(data.items[0]);
+      loadRecommendedVideos(data.items[0].snippet.title);
+    }
+  } catch (error) {
+    console.error("Error fetching channel info: ", error);
+  }
+}
+
+function displayChannelInfo(channelData) {
+  const channelInfoSection = document.getElementById("channel-info");
+  channelInfoSection.innerHTML = `
+        <h3>${channelData.snippet.title}</h3>
+        <img src="${channelData.snippet.thumbnails.default.url}" alt="${channelData.snippet.title}">
+        <p>${channelData.snippet.description}</p>
+    `;
+}
+
+async function loadComments(videoId) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/commentThreads?key=${API_KEY}&videoId=${videoId}&maxResults=25&part=snippet`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("comments", data);
+    if (data.items) {
+      displayComments(data.items);
+    } else {
+      console.log("No comments available or data is undefined.");
+    }
+  } catch (error) {
+    console.error("Error fetching comments: ", error);
+  }
+}
+
+function displayComments(comments) {
+  const commentSection = document.getElementById("comment-section");
+  const commentsContainer = document.getElementById("comments-container");
+  commentsContainer.innerHTML = "";
+
+  comments.forEach((comment) => {
+    const commentText = comment.snippet.topLevelComment.snippet.textDisplay;
+    console.log("COMMENT TEXT", commentText);
+
+    //Getting user profiles
+    for (let i = 0; i < profilesArray.length; i++) {
+      let profile = profilesArray[i];
+      console.log("PROFILE", profile);
+      showProfile(profile);
+    }
+    function showProfile(profile) {
+      const profileElement = document.createElement("div");
+      //   const commentElement = document.createElement("p");
+      profileElement.className = "profile-container";
+      const commentDiv = document.createElement("div");
+
+      profileElement.innerHTML = `
+    <img src=${profile} alt="profile"/>
+    <p class="title">${commentText}</p>
+
+    
+    `;
+      
+
+      commentsContainer.appendChild(profileElement);
+      commentsContainer.appendChild(commentDiv);
+    }
+  });
+}
+
+async function loadRecommendedVideos(channelName) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/search?key=${API_KEY}&maxResults=20&part=snippet&q=${channelName}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Recommended videos", data);
+    if (data.items) {
+      displayRecommendedVideos(data.items);
+    } else {
+      console.log("No recommended videos available or data is undefined.");
+    }
+  } catch (error) {
+    console.error("Error fetching recommended videos: ", error);
+  }
+}
+
+function displayRecommendedVideos(videos) {
+  const recommendedSection = document.getElementById("recommended-videos");
+  recommendedSection.innerHTML = "";
+
+  videos.forEach((video) => {
+    const videoId = video.id.videoId;
+    const title = video.snippet.title;
+    const thumbnail = video.snippet.thumbnails.default.url;
+    const videoCard = document.createElement("div");
+    videoCard.className = "video-card";
+    videoCard.innerHTML = `
+        <div>
+            <a href="video.html?videoId=${videoId}" class="rec-link">
+                <img src="${thumbnail}" alt="${title}">
+            </a>
+            </div>
+            <div>
+            <p class="rec-heading">${title}</p>
+            </div>
+
+        `;
+    recommendedSection.appendChild(videoCard);
+  });
+}
+
+/*searchBtn.addEventListener("click",()=>{
+  let searchBarEle=document.getElementById("searchBar");
+  let userSearching=searchBarEle.value;
+  console.log("USER SEARCHING",userSearching);
+  displayRecommendedVideos(userSearching);
+})*/
